47ce9f1a909c304c4cc9d42639d5fa6327bf4256
psave <paulsave01@gmail.com>
2016-03-29 13:40:48 -0700

    getting login actually working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 RottenMangoes::Application.routes.draw do
-  get "users/new"
-  get "users/create"
-  get "sessions/new"
-  get "sessions/create"
+
   resources :movies
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
