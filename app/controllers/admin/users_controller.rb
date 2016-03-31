545cf4fa1c087dd00c784cb9a8218dab4a99bfee
psave <paulsave01@gmail.com>
2016-03-30 22:14:28 -0700

    adding ability to edit and delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,33 +2,56 @@ class Admin::UsersController < ApplicationController
 
   before_action :restrict_admin_access, :except => [:login]
 
-  def login
-    @user = User.new
-    #@skip_footer = true
-  end
+  # def login
+  #   @user = User.new
+  #   #@skip_footer = true
+  # end
 
   # def new
   #   @user = User.new
   #   render 'users/admin'
   # end
+  def index
+    @users = User.page(params[:page]).per(10)
+  end
 
   def new
     @user = User.new
   end
 
-  def index
-    @users = User.page(params[:page]).per(10)
-  end
-
   def create
     @user = User.new(user_params)
-    @user.save
+
+    if @user.save
+      redirect_to admin_users_path, notice: "#{@user.full_name} created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
   end
 
   protected
 
   def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
